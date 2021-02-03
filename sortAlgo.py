2eb3c2bc50994d00266251cf7e46106f0bb7fdf4
boy56 <duanjian56@126.com>
2021-02-03 19:45:29 +0800

    add binarySearch algo

--- sortAlgo.py
+++ sortAlgo.py
@@ -1,6 +1,7 @@
 # 排序类算法
 
 # 快排模板
+# leetcode: 215题, 480题
 def quik_sort(nums, left, right):
     if left >= right:
         return
@@ -21,9 +22,24 @@ def quik_sort(nums, left, right):
     quik_sort(nums, start, left - 1)
     quik_sort(nums, left + 1, end)
 
+# 二分查找, nums为递增数组
+# leetcode: 34题, 480题
+def binarySearch(nums, left, right, n):
+    if left > right: return -1
+
+    mid_pos = (left + right)//2
+    # print(mid_pos)
+    mid_value = nums[mid_pos]
+    if mid_value == n:
+        return mid_pos
+    elif mid_value > n:
+        return binarySearch(nums, left, mid_pos - 1, n)
+    else:
+        return binarySearch(nums, mid_pos+1, right, n)
 
 
 if __name__ == "__main__":
     nums = [3, 7, 6, 3]
     quik_sort(nums, 0, 3)
     print(nums)
+    print(binarySearch(nums, 0, 3, 3))
